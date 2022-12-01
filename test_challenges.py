6ade0f95eb57321430e4f55d3e2cf5952da64195
Jorge Saura <jorge.saura@live.com>
2022-12-01 15:10:04 +0100

    Initial commit

---
+++ test_challenges.py
@@ -0,0 +1,13 @@
+import unittest
+
+import challenges 
+
+class TestBasics(unittest.TestCase):
+    def test_t(self):
+        self.assertTrue(1,1)    
+
+
+
+if __name__ == '__main__':
+
+    unittest.main()
