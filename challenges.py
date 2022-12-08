a49010e416c0f12e7b21f40831502485cfcba026
Jorge Saura <jorge.saura@live.com>
2022-12-08 12:26:09 +0100

    Advent of code Day 9. Treetop Tree Houser. Part I

--- challenges.py
+++ challenges.py
@@ -363,7 +363,58 @@ class FileSysteNavigator:
             return min([x for x in self.all_dirs.values() if total_system_space - (space_ocuppied - x) >= min_space_required])
 
         
+#--- Day 8: Treetop Tree House ---
 
+import numpy as np
+
+class TreeVisibility:
+    
+    tree_grid = None
+    tree_grid_transposed = NotImplementedError
+    
+    def _decode_data(self, input:str)-> list[str]:
+        result = []
+        lines = input.split('\n')
+        for line in lines:
+            result.append(list(line))
+        return result
+    
+    def _is_visible_horizontally(self, tree:int, tree_grid:list[str], point:tuple()) -> bool:
+        idx_h, idx_v = point
+
+        max_left = int(max(tree_grid[idx_v][0:idx_h]))
+        if max_left < tree:
+            return True
+
+        max_right = int(max(tree_grid[idx_v][idx_h+1:]))
+        if max_right < tree:
+            return True
+
+        return False
+
+    def _is_visible_vertically(self, tree:int, tree_grid:list[str], position:tuple()) -> bool:
+        position = (position[1],position[0])
+        return self._is_visible_horizontally(tree, tree_grid,position)
+
+    def get_number_of_visible_trees(self, input:str) -> int:
+
+        self.tree_grid = self._decode_data(input)
+        self.tree_grid_transposed = np.transpose(self.tree_grid).tolist()
+        num_visible_trees = (len(self.tree_grid) + len(self.tree_grid[0]) -2 ) * 2 # the edges
+
+        for idx_v, line in enumerate(self.tree_grid [1:-1]):
+            for idx_h, tree in enumerate(line[1:-1]):
+                point = (idx_h+1, idx_v+1)
+                #mira hacia arriba
+                #mira hacia abajo
+                #mira izqd
+                #mira derecha
+                #si alguno es true, no continues
+                #actualiza el contador
+                if self._is_visible_horizontally(int(tree), self.tree_grid,point):
+                    num_visible_trees += 1
+                elif self._is_visible_vertically(int(tree), self.tree_grid_transposed,point):
+                   num_visible_trees += 1
         
-
+        return num_visible_trees
 
--- test_challenges.py
+++ test_challenges.py
@@ -122,6 +122,19 @@ class TestChallenges(unittest.TestCase):
         self.assertEqual(f.get_smallest_deletable_directory(30000000),24933642)
 
 
+    def test_tree_visibility(self):
+
+        tv = challenges.TreeVisibility()
+
+        grid = tv._decode_data(data.tree_grid_simple)
+
+        self.assertEqual(tv._is_visible_horizontally(2,grid,(1,1)), True)
+        self.assertEqual(tv._is_visible_horizontally(0,grid,(1,1)), False)
+        
+
+        self.assertEqual(tv.get_number_of_visible_trees(data.tree_grid_simple),8)
+        self.assertEqual(tv.get_number_of_visible_trees(data.tree_grid),21)
+
 
         
 
