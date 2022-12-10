4b1019d57aacaa43458700e641d7f3d7c7f882e6
Jorge Saura <jorge.saura@live.com>
2022-12-10 11:03:23 +0100

    Advent of Code Day 10. Cathode-Ray Tube. Part I

--- challenges.py
+++ challenges.py
@@ -468,7 +468,6 @@ class Rope:
             (-1,-1),(0,-1),(1,-1)
             ]
 
-
     def _decode_movements(self, input:str) -> list[tuple]:
         lines = input.split('\n')
         result = []
@@ -550,3 +549,73 @@ class Rope:
 
 
         return len(tail_visited)
+
+
+#--- Day 10: Cathode-Ray Tube ---
+
+class Instruction:
+    type_op = 'noop'
+    duration = 1
+    value = 0 
+    
+    def __init__(self, type_op:str, duration:int, value:int) -> None:
+        self.type_op = type_op
+        self.duration = duration
+        self.value = value
+        
+
+class CathodeRayTube:
+
+    def _decode_instruction(self, input:str) -> Instruction:
+        if input.startswith("noop"):
+            return Instruction("noop",1,0)
+        
+        if input.startswith("addx"):
+            instruction, value = input.split()
+            return Instruction(instruction,2,int(value))
+
+    def _decode_instructions(self, input:str) -> list[Instruction]:
+        lines = input.split('\n')
+        #(op. type, op. duration, op. value)
+        instructions = []
+
+        for line in lines:
+            instruction = self._decode_instruction(line)
+            if instruction.type_op == 'addx':
+                instructions.append(Instruction('noop',1,0))
+                instructions.append(Instruction('add',1,instruction.value))
+            else:
+                instructions.append(instruction)
+
+        return instructions
+
+    def execute_program(self, input:str) -> int:
+        program = self._decode_instructions(input)
+
+        cycle = 1
+        X_value = 1
+
+        cycles_inspected = dict()
+        next_cycle_to_inspect = 20
+        sum_cycles = 40
+
+        for ins in program:
+            if cycle == next_cycle_to_inspect:
+                cycles_inspected[cycle] = X_value
+                next_cycle_to_inspect += sum_cycles
+                
+            cycle += 1
+            if ins.type_op == 'add':
+                X_value += ins.value
+
+        return sum(x*y for x,y in cycles_inspected.items()) if cycles_inspected else 0
+
+
+
+
+
+
+
+
+
+
