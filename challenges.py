b5bc1e359bc27cd32d7de8539118c797cff368a0
Jorge Saura <jorge.saura@live.com>
2022-12-13 16:05:30 +0100

    Advent of Code Day 13. Distress Signal. Part I

--- challenges.py
+++ challenges.py
@@ -855,4 +855,133 @@ class HillClimbing:
 
         return results
 
+
+#--- Day 13: Distress Signal ---
+
+class DistressSignal:
+    
+    def _transforn_to_list(self, string:str) -> list:
+
+        if string.isnumeric():
+            return int(string)
         
+        if string.startswith('['): string = string [1:]
+        if string.endswith(']'): string = string [:-1]
+
+        elements = list()
+        
+        while string:
+            if string[0].isdigit():
+                first_coma = string.find(',')
+                if first_coma > 0:
+                    num = string[:first_coma]
+                    string = string.lstrip(num)
+                else:
+                    num = string
+                    string = string.lstrip(num)
+                elements.append(num)
+            elif string[0] == '[':
+                indexes = [i for i, ltr in enumerate(string) if ltr == ']']
+                
+                good_index = 0
+                for idx in indexes:
+                    part = string[:idx+1]
+                    if part.count('[') == part.count(']'):
+                        good_index = idx
+                        break
+                
+                elements.append(string[:good_index + 1])
+                string = string[good_index + 1:]
+            elif string[0] == ',':
+                string = string.lstrip(',')
+
+
+        result = [self._transforn_to_list(x) for x in elements]
+        return result
+
+
+    def _decode_input(self, input:str)->list[tuple[list,list]]:
+        pairs = input.split('\n\n')
+
+        result = list()
+        for pair in pairs:
+            pair1, pair2 = pair.split('\n')
+
+            list1 = self._transforn_to_list(pair1)
+            list2 = self._transforn_to_list(pair2)
+
+            result.append((list1,list2))
+
+        return result
+
+    def _get_first_item_in_nested_list(self, nested_list:list) -> int:
+
+        while isinstance(nested_list,list):
+            if nested_list:
+                nested_list = nested_list[0]
+            else:
+                nested_list = None
+
+        return nested_list
+
+    def _check_right_order(self, list1:list, list2:list) -> bool:
+        
+
+        result = None
+
+        for ele1, ele2 in zip(list1, list2):
+            if isinstance(ele1, list) and isinstance(ele2, list):
+                                  
+                result = self._check_right_order(ele1, ele2)
+
+            if isinstance(ele1, list) and isinstance(ele2, int):
+             
+                result =  self._check_right_order(ele1, [ele2])
+
+                # first_item = self._get_first_item_in_nested_list(ele1)
+                # result = first_item <= ele2 if first_item else True
+
+            if isinstance(ele1, int) and isinstance(ele2, list):
+                result = self._check_right_order([ele1], ele2)
+
+                # first_item = self._get_first_item_in_nested_list(ele2)
+                # result = ele1 <= first_item if first_item else False
+            
+            if isinstance(ele1, int) and isinstance(ele2, int):
+
+                if ele1 < ele2:
+                    result = True
+                if ele1 > ele2:
+                    result = False
+
+            if result != None:
+                break
+
+        if result == None:
+            if len(list1) > len(list2):
+                result = False
+            if len(list1) < len(list2): 
+                result = True
+                
+        return result
+
+
+    def sum_indexes_right_order(self, input:str) -> int:
+        list_pairs = self._decode_input(input)
+        sum_of_index = 0
+        for idx, pair in enumerate(list_pairs):
+            list1, list2 = pair
+            try:
+                is_in_right_order =  self._check_right_order(list1,list2)
+                if is_in_right_order != None:
+                    sum_of_index += (idx + 1) if is_in_right_order else 0
+            except:
+                
+                print(list1)
+                print(list2)
+
+
+
+        return sum_of_index
+
+
