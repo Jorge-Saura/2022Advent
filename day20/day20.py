98e4132cecbe5b1e6ff67bd91782cbf819b081ed
Jorge Saura <jorge.saura@live.com>
2023-01-27 16:28:46 +0100

    Advent of Code Day 20. Grove Positioning System. Refactoring Part I after found some errors.

--- day20/day20.py
+++ day20/day20.py
@@ -13,31 +13,85 @@ class DecrypterOperator:
 
         return numbers
 
+    def newMod(self, a, b):
+        res = a%b
+        return res if not res else res-b if a<0 else res
 
     def decrypt(self, numbers:list[int]) -> list[int]:
 
         pairs = [[idx, num] for idx, num in enumerate(numbers)]
-        length = pairs[-1][0]
+        length = len(pairs) - 1
 
 
         for p in pairs:
             idx, num = p
+            if num == 0:
+                continue
 
-            new_index = (idx + num) % length
-            new_index = new_index if new_index > 0 else length-new_index
-            
-
-            for x in pairs:
-                current_idx, _ = x
+            movement = idx + num
+          
+            if num > 0:
+                new_index = ((movement -1) % length) + 1
                 if new_index > idx:
-                    x[0] = current_idx - 1 if idx < current_idx <= new_index else current_idx
-                if new_index < idx:
-                    x[0] = current_idx + 1 if new_index <= current_idx <= idx else current_idx
+                    for i,x in enumerate(pairs):
+                        current_idx, _ = x
+                        pairs[i][0] = current_idx - 1 if idx < current_idx <= new_index else current_idx
+                elif new_index < idx :
+                    for i,x in enumerate(pairs):
+                        current_idx, _ = x
+                        pairs[i][0] = current_idx + 1 if new_index <= current_idx < idx else current_idx
+
+            elif num < 0:
+                if movement < 0:
+                    real_mov = self.newMod(movement,length)
+                    new_index = length + real_mov 
+                elif movement == 0:
+                    new_index = length
+                else:
+                    new_index = movement
+
+                if new_index > idx:
+                    for i,x in enumerate(pairs):
+                        current_idx, _ = x
+                        pairs[i][0] = current_idx - 1 if idx < current_idx <= new_index else current_idx
+                    
+                elif new_index < idx :
+                    for i,x in enumerate(pairs):
+                        current_idx, _ = x
+                        pairs[i][0] = current_idx + 1 if new_index <= current_idx < idx else current_idx
+            
             p[0] = new_index
 
         pairs.sort(key = lambda x: x[0])
         
         return [x for idx, x in pairs]
+
+
+    def decrypt2(self, numbers:list[list[int]]) -> list[list[int]]:
+
+        numbers.sort(key= lambda x: x[2])
+        length = len(numbers)
+
+
+        for triplet in numbers:
+            idx, num, order = triplet
+
+            new_index = (idx + num) % length
+            # new_index = new_index if new_index >= 0 else length-new_index
+            
+
+            for x in numbers:
+                current_idx, _, _ = x
+                if new_index > idx:
+                    x[0] = current_idx - 1 if idx < current_idx <= new_index else current_idx
+                if new_index < idx:
+                    x[0] = current_idx + 1 if new_index <= current_idx <= idx else current_idx
+            triplet[0] = new_index
+
+        numbers.sort(key = lambda x: x[0])
+        
+        return numbers
+
 
 
     def get_coordenates(self, input:str) -> int:
@@ -53,3 +107,18 @@ class DecrypterOperator:
         return first_coor + second_coor + third_coor
 
 
+    def get_coordenates2(self, input:str) -> int:
+        numbers = self._decode_input(input)
+        decrypted_numbers = [[idx, x * 811589153, idx] for idx, x in enumerate(numbers)]
+        # decrypted_numbers = self.decrypt2(numbers)
+        for _ in range(10):
+            decrypted_numbers = self.decrypt2(decrypted_numbers)
+        
+        length = len(decrypted_numbers)
+        zero_idx = decrypted_numbers.index(0)
+        first_coor = decrypted_numbers[(zero_idx + 1000) % length]
+        second_coor = decrypted_numbers[(zero_idx + 2000) % length]
+        third_coor = decrypted_numbers[(zero_idx + 3000) % length]
+
+        return first_coor + second_coor + third_coor
+ 
--- day20/test_day20.py
+++ day20/test_day20.py
@@ -17,6 +17,33 @@ class TestDay20(unittest.TestCase):
     def test_decryption(self):
 
         do = challenges.DecrypterOperator()
+
+
+        self.assertEqual(do.decrypt([1,0,0]), [0,1,0])
+        self.assertEqual(do.decrypt([0,1,0]), [0,0,1])
+        self.assertEqual(do.decrypt([0,0,1]), [0,1,0])
+
+        self.assertEqual(do.decrypt([-1,0,0]), [0,-1,0])
+        self.assertEqual(do.decrypt([0,-1,0]), [0,0,-1])
+        self.assertEqual(do.decrypt([0,0,-1]), [0,-1,0])
+
+
+        self.assertEqual(do.decrypt([2,0,0]), [0,0,2])
+        self.assertEqual(do.decrypt([0,2,0]), [0,2,0])
+        self.assertEqual(do.decrypt([0,0,2]), [0,0,2])
+
+        self.assertEqual(do.decrypt([-2,0,0]), [0,0,-2])
+        self.assertEqual(do.decrypt([0,-2,0]), [0,-2,0])
+        self.assertEqual(do.decrypt([0,0,-2]), [0,0,-2])
+
+        self.assertEqual(do.decrypt([-3,0,0]), [0,-3,0])
+
+        self.assertEqual(do.decrypt([-4,0,0]), [0,0,-4])
+        self.assertEqual(do.decrypt([5,0,0]), [0,5,0])
+        self.assertEqual(do.decrypt([0,5,0]), [0,0,5])
+
+        self.assertEqual(do.decrypt([0,-3,0,0,0,0,0]), [0,0,0,0,-3,0,0])
+
         l = do._decode_input(data.encrypted_basic)
         self.assertEqual(do.decrypt(l), [1, 2, -3, 4, 0, 3, -2])
        
@@ -24,7 +51,7 @@ class TestDay20(unittest.TestCase):
 
         self.assertEqual(do.get_coordenates(data.encrypted_file), 10763)
 
-
+        # self.assertEqual(do.get_coordenates2(data.encrypted_basic), 1623178306)
 
 if __name__ == "__main__":
 
